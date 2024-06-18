f3c0ed50c950da0559bd6ee41c12f048c79653a9
Mustafa Muhammed <mustafa860505@hotmail.com>
2024-06-18 22:33:31 +0000

    Initial js setup with country array and flag display test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
 // Wait for the DOM to finish loading before running the game
 document.addEventListener('DOMContentLoaded', function () {
-  console.log('Document loaded');
+  let country = getRandomCountry();
+  document.getElementById(
+    'flag-image'
+  ).src = `https://flagcdn.com/h160/${country.code}.png`;
 });
+
+// Declare initial variables
+let correctCount = 0;
+let incorrectCount = 0;
+let timeRemain = 15; // set countdown time to 15 seconds
+
+const countries = [
+  { code: 'at', name: 'Austria' },
+  { code: 'be', name: 'Belgium' },
+  { code: 'bg', name: 'Bulgaria' },
+];
+
+// Function to get a random country from the countries array
+
+function getRandomCountry() {
+  let randomCountry = Math.floor(Math.random() * countries.length);
+  return countries[randomCountry];
+}
